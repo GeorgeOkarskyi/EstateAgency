7e73729b3b22b1dac9bc0dba712fff84f00fd5e3
George <goshan771@gmail.com>
2019-12-24 19:25:13 +0200

    files strucrure was created

---
+++ src/components/header/navigation/navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import styles from './navigation.module.css';
+
+const Navigation = () => {
+    return (
+        <div >
+            <nav className={styles.navigation}> 
+                <div className="container">
+                    <h2>dfghgf</h2>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
---
+++ src/components/header/slider/slider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './slider.module.css';
+
+
+const Slider = () =>{
+    return(
+        <div >
+            <h1 className={styles.title}>
+                Slider
+            </h1>
+        </div>
+    )
+}
+
+export default Slider
